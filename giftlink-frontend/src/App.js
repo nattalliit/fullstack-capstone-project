757f133b0b089a7f9211b3fdd386363122ddbfd4
nattalliit <91556919+nattalliit@users.noreply.github.com>
2024-04-23 16:58:42 +0300

    Edit front-end App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -10,9 +10,10 @@ import Navbar from './components/Navbar/Navbar';
 function App() {
   const navigate = useNavigate();
   return (
-<>
+    <>
         <Navbar/>
         <Routes>
+          {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/login" element={<LoginPage/>} />
