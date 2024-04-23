31b74bd8259199a302fb74a0a2978845e5c4ff6a
nattalliit <91556919+nattalliit@users.noreply.github.com>
2024-04-23 16:29:12 +0300

    Create API for Search Service

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,66 +3,48 @@ require('dotenv').config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const path = require('path');
-
 const pinoLogger = require('./logger')
-
 const connectToDatabase = require('./models/db');
-
 const app = express();
 const port = 3050;
-
 // Serve static files from the public directory (for home.html)
 app.use(express.static(path.join(__dirname, 'public')));
-
 // Serve static files for React App from a subdirectory
 app.use('/app', express.static(path.join(__dirname, 'public', 'react-app')));
-
-
 // Route for Home Page - Serve home.html as the default page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
-
 // Serve the React app's index.html for any other requests under /app
 app.get('/app/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'react-app', 'index.html'));
 });
-
-
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
     .catch((e) => console.error('Failed to connect to DB', e));
-
-
 app.use(express.json());
-
 // Route files
 const giftRoutes = require('./routes/giftRoutes');
 const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
-
 app.use(pinoHttp({ logger }));
-
 // Use Routes
 app.use('/api/gifts', giftRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/search', searchRoutes);
-
 // Default Route for React App
 app.get('/app/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'react-app', 'index.html'));
 });
-
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
-
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
